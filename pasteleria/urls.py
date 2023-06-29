495985daa4a9f46c8e6735deab28811f05376a8f
edisinh0 <ed.cerpa@duocuc.cl>
2023-06-29 00:24:46 -0400

    Se agrego orm

--- pasteleria/urls.py
+++ pasteleria/urls.py
@@ -1,7 +1,11 @@
 #from django.conf.urls import url
-from django.urls import path 
-from . import views
+from django.urls import path
+from . import views 
 
 urlpatterns = [
-    path('index', views.index, name='index'),
+    path('index', views.index, name='Index'),
+    path('contacto', views.contacto, name='Contacto'),
+    path('catalogo', views.catalogo, name='catalogo'),
+    path('login', views.login, name='login'),
+    path('registro', views.registro, name='registro'),
 ]
--- pasteleria/views.py
+++ pasteleria/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
-
+from .models import Alumno,Genero
 # Create your views here.
 
 def index(request):
-    context={}
-    return render(request,'pasteleria/index.html', context)
+    alumnos= Alumno.objects.all()
+    context={"alumnos":alumnos}
+    return render(request,'index.html', context)
+
+
+def contacto(request):
+    return render(request,'contacto.html')
+
+def catalogo(request):
+    return render(request,'catalogo.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def registro(request):
+    return render(request,'registro.html')
