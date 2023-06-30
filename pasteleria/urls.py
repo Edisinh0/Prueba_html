042e2e6b8cb7468ce46d7714895cb493cb0ea4fe
edisinh0 <ed.cerpa@duocuc.cl>
2023-06-29 22:26:17 -0400

    Se agrego el crud

--- pasteleria/urls.py
+++ pasteleria/urls.py
@@ -1,5 +1,5 @@
 #from django.conf.urls import url
-from django.urls import path
+from django.urls import path, include
 from . import views 
 
 urlpatterns = [
@@ -8,4 +8,10 @@ urlpatterns = [
     path('catalogo', views.catalogo, name='catalogo'),
     path('login', views.login, name='login'),
     path('registro', views.registro, name='registro'),
+    path('crud', views.crud, name='crud'),
+    path('alumnosAdd', views.alumnosAdd, name='alumnosAdd'),
+    path('alumnos_findEdit/<str:pk>', views.alumnos_findEdit, name='alumnos_findEdit'),
+    path('alumnos_del/<str:pk>', views.alumnos_del, name='alumnos_del'),path('alumnos_del/<str:pk>', views.alumnos_del, name='alumnos_del'),
+    path('accounts/', include('django.contrib.auth.urls'))
+
 ]
--- pasteleria/views.py
+++ pasteleria/views.py
@@ -15,7 +15,83 @@ def catalogo(request):
     return render(request,'catalogo.html')
 
 def login(request):
-    return render(request,'login.html')
+    return render(request,'registration/login.html')
 
 def registro(request):
-    return render(request,'registro.html')
+    return render(request,'registration/registro.html')
+
+def crud(request):
+    alumnos = Alumno.objects.all()
+    context = {"alumnos": alumnos}
+    return render(request, 'alumnos_list.html', context)
+
+def alumnosAdd(request):
+    if request.method != "POST":
+        # no es un POST por lo tanto se muestra el formulario para agregar
+        generos = Genero.objects.all()
+        context = {"generos": generos}
+        return render(request, 'alumnos_add.html', context)
+    
+    else:
+        print("--->>>> llego al else de addAlumnos crea el objeto ")
+        # Es un POST, por lo tanto se recuperan los datos del formulario
+        # y se graban en la tabla
+        rut = request.POST["rut"]
+        nombre = request.POST["nombre"]
+        aPaterno = request.POST["paterno"]
+        aMaterno = request.POST["materno"]
+        fechaNac = request.POST["fechaNac"]
+        genero = request.POST["genero"]
+        telefono = request.POST["telefono"]
+        email = request.POST["email"]
+        direccion = request.POST["direccion"]
+        activo = "1"  # ---> esta linea no será tomada en cuenta en la creación dado que se asigna 1 arbitrariamente
+
+        objGenero = Genero.objects.get(id_genero=genero)
+        obj = Alumno.objects.create(rut=rut,
+                                    nombre=nombre,
+                                    apellido_paterno=aPaterno,
+                                    apellido_materno=aMaterno,
+                                    fecha_nacimiento=fechaNac,
+                                    id_genero=objGenero,
+                                    telefono=telefono,
+                                    email=email,
+                                    direccion=direccion,
+                                    activo=1)
+        obj.save()
+        context = {"mensaje": "OK, datos grabados..."}
+        return render(request, 'alumnos_add.html', context)
+    
+def alumnos_del(request, pk):
+    context = {}
+    try:
+        alumno = Alumno.objects.get(rut=pk)
+        print("REGISTRO A ELIMINAR============>>>")
+        print(alumno)
+        alumno.delete()
+        print("ELIMINADOR============>>>")
+        mensaje = "Bien, dato eliminado!!!"
+        alumnos = Alumno.objects.all()
+        context = {"alumnos": alumnos, "mensaje": mensaje}
+        return render(request, "alumnos_list.html", context)
+    except:
+        mensaje = "Error, el rut  no existe!!!"
+        alumnos = Alumno.objects.all()
+        context = {"alumnos": alumnos, "mensaje": mensaje}
+        return render(request, "alumnos_list.html", context)
+    
+def alumnos_findEdit(request, pk):
+    if pk != "":
+        alumno = Alumno.objects.get(rut=pk)
+        print(alumno)
+        generos = Genero.objects.all()
+        print("generos--->>>")
+        print(generos)
+
+        print(type(alumno.id_genero.genero))
+        context = {"alumno": alumno, "generos": generos}
+        if alumno:
+            return render(request, "alumnos_edit.html", context)
+        else:
+            context = {"mensaje": "Error, rut no existe"}
+            return render(request, "alumnos_list.html", context)
